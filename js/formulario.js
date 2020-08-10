5369abe9a01b7952df65f1c23d60a211e978e3e6
Carlos Villatoro <66433870+Snake-oft@users.noreply.github.com>
2020-08-09 21:23:43 -0600

    Se actualizaron datos
    pagina

--- js/formulario.js
+++ js/formulario.js
@@ -1,25 +1,35 @@
-function validar(evento) {
-evento.preventDefault();
+function datos(e){
+e.preventDefault();
 
-var user = ["gato@correo.com", "perro@correo.com", "tomate@correo.com"];
-var password = ["A1234", "B1234", "C1234"];
+var users=["gato@correo.com","perro@correo.com","tomate@correo.com"];
+var password=["A1234","B1234","C1234"];
 
-var eCorreo = document.querySelector("#correo").value;
-var eContrasena = document.querySelector("#password").value;
+var correo= document.querySelector("#user").value;
+var pass= document.querySelector("#contra").value;
 
-for(var i=0;i<=2;i++){
-    if (eCorreo===user[i]) {
-        if (eContrasena===password[i]) {
-        idTituloMensaje.textContent = "Welcome ";
-            } else if (eContrasena!== password[i]) {
-        idTituloMensaje.textContent="User Not Found";
+var idcorreo = document.querySelector("#uno");
+var idpass = document.querySelector("#dos");
+var contador = 0;
 
-        }   
-    }
+    for(var i=0;i<=2;i++){
+      if(correo == users[i]){
+        idcorreo.innerHTML ="<p> Correct E-mail</p>";
+
+		      if(pass == password[i]){
+			       idpass.innerHTML ="<p> Correct Password</p>";
+
+			return
+		}else{
+			       idpass.innerHTML ="<p> Want your pass back?</p>";
+			return
+		}
+	}
 }
-    
-/**/
+
+	idcorreo.innerHTML ="<p> Invalid User</p>";
+	document.getElementById("cuenta").style.fontStyle="italic";
+  document.getElementById("cuenta").style.fontSize="17px";
 }
-var boton_iniciar_sesion = document.querySelector("#iniciar");
-boton_iniciar_sesion.addEventListener("click", validar);
-var idTituloMensaje = document.querySelector("#CorreoInvalido");
+
+var boton =document.querySelector("#enviar");
+boton.addEventListener("click",datos);
